73a11f7cdd10c2f76edf83a91538c500e0f30ef8
juyun <kimj4@carleton.edu>
2017-10-11 15:40:40 -0500

    code formatting, output formatting added. Writing word frequencies to a csv added

--- Assignment2/assignment2.py
+++ Assignment2/assignment2.py
@@ -6,8 +6,27 @@ import pprint
 import random
 import copy
 from decimal import *
+import csv
+import operator
+import collections
+
+
+
 nltk.download('punkt')
-# tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+
+testSentences = ["Please return the television to Mr. Lynch on Sunday.",
+"Odysseus was 17 when he first had novocain.",
+"Johnny Cash was drowning in imagery of dusty roads at night.",
+"While wearing blue shoes, I took a detour to find Laura Frost, the Wizard of Python Empire.",
+"I built a box for the rabbits.",
+"She was wearing a leather jacket.",
+"I was born in 1976 and graduated high school in 1990.",
+"That explanation is irrelevant to surrealists, existentialists and film snobs for purposes of interpretation, narrative form, humorous asides and soap.",
+"Sentence of your choosing.",
+"Another sentence of your choosing."]
+
+
 
 def dataSplit(trainingRatio, sentences):
     ''' splits up the data into training and test sets where trainingRatio
@@ -31,7 +50,7 @@ def calculatePerplexity(N, trainingAndTestSets):
     gramDict = {}
     total1Grams = 0
 
-    # special case for handling unigrams
+    # first create the ngram models
     for i in range(1, N + 1):
         trainingGrams = createNgram(i, trainingAndTestSets[0])
         gramDict[str(i) + "gram"] = trainingGrams
@@ -41,45 +60,47 @@ def calculatePerplexity(N, trainingAndTestSets):
             total1Grams = sum(gramDict['1gram'].values())
 
 
+    # stores the perplexity of sentences so they can be averaged later
     sentencePerplexityList = []
-    # TODO iterate through 1 through N
+    # so calculate perplexity for each sentence
     for sentence in trainingAndTestSets[1]:
         sentenceNgrams = createNgram(N, [sentence])
         sentenceProbability = 1
 
         for ngram, count in sentenceNgrams.items():
             if (N == 1):
-                # unigram probability is the count of that unigram / count of all unigrams
+                # unigram probability = this unigram count / all unigram count
                 ngramProbability = count / total1Grams
             else:
-                # get count of preceding words in ngram
-                prevWords = ngram.split(' ')[0:N-1]
-                prevWords = ' '.join(prevWords)
-                # TODO change n to curval when you change the prev todo
-                prevWordsCount = 0
-                if prevWords in gramDict[str(N - 1) + "gram"]:
-                    prevWordsCount = gramDict[str(N - 1) + "gram"][prevWords]
+                # ngram probability = this ngram count / this n-1gram count
+                # make the n-1gram by popping off the last word in ngram
+                prevGram = ngram.split(' ')[:-1]
+                prevGram = ' '.join(prevGram)
+
+                prevGramCount = 0
+                if prevGram in gramDict[str(N - 1) + "gram"]:
+                    prevGramCount = gramDict[str(N - 1) + "gram"][prevGram]
                 else:
-                    if prevWords.split(' ')[-1] == "<s>":
-                        prevWordsCount = gramDict["1gram"]['<s>']
+                    # special case for handling something like <s> <s> I
+                    #  just use the <s> count from unigrams
+                    if prevGram.split(' ')[-1] == "<s>":
+                        prevGramCount = gramDict["1gram"]['<s>']
 
-                # if there's no prevWordsCount, there will be no ngram count because
+                # if there's no prevGramCount, there will be no ngram count because
                 # that sequence of words never appears in the training set
-
 
                 if ngram in gramDict[str(N) + "gram"]:
                     trainingCount = gramDict[str(N) + "gram"][ngram]
-
-                    ngramProbability = trainingCount / prevWordsCount
+                    ngramProbability = trainingCount / prevGramCount
                 else:
+                    # arbitary small probability if the ngram is not in the training set
                     ngramProbability = 0.000001
 
             sentenceProbability = Decimal(sentenceProbability * Decimal(ngramProbability))
 
-        sentencePerplexity = (1/sentenceProbability)**Decimal(1/N)
+        sentencePerplexity = (1 / sentenceProbability) ** Decimal(1 / N)
         sentencePerplexityList.append(sentencePerplexity)
     perplexity = sum(sentencePerplexityList) / len(sentencePerplexityList)
-    # print(perplexity)
     return perplexity
 
 
@@ -118,23 +139,35 @@ def createNgram(N, sentences):
 
     return nGrams
 
+def writeToCSV(wordDict, filename):
+    '''
+    for making word frequency visualizations
+    Takes wordDict, sorts it by value in descending or into a list of tuples,
+        writes each tuple as a row in the csv
+    '''
+    # TODO: There is a bit of a problem when the word is a comma
+
+    sortedList = sorted(wordDict.items(), key=lambda kv: kv[1], reverse=True)
+    with open(filename, 'w') as f:
+        for line in sortedList:
+            f.write(str(line[0]) + ',' + str(line[1]) + '\n')
+
 def main():
     punkt_param = PunktParameters()
     punkt_param.abbrev_types = set(['dr', 'vs', 'mr', 'mrs', 'prof', 'inc'])
     sentence_splitter = PunktSentenceTokenizer(punkt_param)
 
+    # fp = open("HW2_InputFile.txt", 'r', encoding='UTF-8')
+    fp = open("HW2_InputFile_NoPunctuation.txt", 'r', encoding='UTF-8')
 
-    fp = open("HW2_InputFile.txt", 'r', encoding='UTF-8')
     data = fp.read()
     sentences = sentence_splitter.tokenize(data)
-    # print ('\n-----\n'.join(tokenizer.tokenize(data)))
 
     numSentences = len(sentences)
     numWords = 0
     wordDict = {}
 
     for sentence in sentences:
-        print (sentence + '\n')
         tokenized_text = nltk.word_tokenize(sentence)
         for word in tokenized_text:
             numWords += 1
@@ -144,29 +177,19 @@ def main():
                 wordDict[word] = 1
 
     numUniqueWords = len(wordDict)
-    # print(len(sentences))
-    print(numSentences)
-    print(numWords)
-    print(numUniqueWords)
-    # pprint.pprint(wordDict)
+    writeToCSV(wordDict, 'wordFrequency.csv')
+    print("number of sentences: %d" % numSentences)
+    print("number of words: %d" % numWords)
+    print("number of unique words: %d" % numUniqueWords)
     # TODO: check if punctuations should be counted as words
     # TODO: check if all of these are words
     # TODO: make visualization for the word frequency
 
-
-    # list containing n-gram models
-    # seventyThirtyGrams = []
-    # eightyTwentyGrams = []
-    # ninetyTenGrams = []
-    #
     seventyThirty = dataSplit(.7, sentences)
     eightyTwenty = dataSplit(.8, sentences)
     ninetyTen = dataSplit(.9, sentences)
-    #
-    # for i in range(1, 4):
-    #     seventyThirtyGrams.append(createNgram(i, seventyThirty[0]))
-    #     eightyTwentyGrams.append(createNgram(i, eightyTwenty[0]))
-    #     ninetyTenGrams.append(createNgram(i, ninetyTen[0]))
+
+    print(' ')
     print('unigram perplexity for 70:30 split: %s' % calculatePerplexity(1, seventyThirty))
     print(' bigram perplexity for 70:30 split: %s' % calculatePerplexity(2, seventyThirty))
     print('trigram perplexity for 70:30 split: %s' % calculatePerplexity(3, seventyThirty))
@@ -178,9 +201,6 @@ def main():
     print('unigram perplexity for 90:10 split: %s' % calculatePerplexity(1, ninetyTen))
     print(' bigram perplexity for 90:10 split: %s' % calculatePerplexity(2, ninetyTen))
     print('trigram perplexity for 90:10 split: %s' % calculatePerplexity(3, ninetyTen))
-
-
-
 
 
 if __name__ == "__main__":

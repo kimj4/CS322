e6170a80d927a5fff0d1e1beae5fa4aecb1a4041
posegae <posegae@carleton.edu>
2017-10-31 15:37:01 -0500

    computes denominator of cosine similarity

--- Assignment4/wordvectors.py
+++ Assignment4/wordvectors.py
@@ -1,17 +1,25 @@
 # http://ronny.rest/blog/post_2017_08_04_glove/
 import linecache
 from random import *
+from math import *
 
 def findCosSimilarity(word1, word2, wordVectorDict):
     num = 0
-    denom = 0
+    denom1 = 0
+    denom2 = 0
     for i in range(0,50):
         num += float(wordVectorDict[word1][i])*float(wordVectorDict[word2][i])
+        denom1 += float(wordVectorDict[word1][i])**2
+        denom2 += float(wordVectorDict[word2][i])**2
+    denom = sqrt(denom1) + sqrt(denom2)
     print(num)
+    print(denom)
+    cosSimilarity = num/denom
+    print(cosSimilarity)
     print('between ' + word1 + ' and ' + word2)
 
 def main():
-    gloveFileName = 'glove.6B/glove.6B.50d.txt'
+    gloveFileName = '/Accounts/posegae/glove.6B/glove.6B.50d.txt'
     numLines = sum(1 for line in open(gloveFileName))
     linesList = []
     while (len(linesList) < 25):
